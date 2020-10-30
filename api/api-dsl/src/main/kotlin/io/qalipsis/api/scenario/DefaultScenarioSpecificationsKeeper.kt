34650efd9b3594f147d28f46088d3c472e38d3ff
Eric Jessé <eric.jesse@aeris-consulting.com>
2020-10-30 17:05:55 +0100

    Add runtime option to filter the scenarios to execute

--- api/api-dsl/src/main/kotlin/io/qalipsis/api/scenario/DefaultScenarioSpecificationsKeeper.kt
+++ api/api-dsl/src/main/kotlin/io/qalipsis/api/scenario/DefaultScenarioSpecificationsKeeper.kt
@@ -1,6 +1,8 @@
 package io.qalipsis.api.scenario
 
+import io.micronaut.context.annotation.Property
 import io.qalipsis.api.context.ScenarioId
+import java.util.Optional
 import javax.inject.Singleton
 
 /**
@@ -9,10 +11,38 @@ import javax.inject.Singleton
  * @author Eric Jessé
  */
 @Singleton
-internal class DefaultScenarioSpecificationsKeeper : ScenarioSpecificationsKeeper {
+internal class DefaultScenarioSpecificationsKeeper(
+    @Property(name = "scenarios-selectors") scenariosSelectors: Optional<String>
+) : ScenarioSpecificationsKeeper {
 
-    override fun asMap(): Map<ScenarioId, ConfiguredScenarioSpecification> {
-        return scenariosSpecifications
+    private val exactMatchers = mutableSetOf<String>()
+    private val patternMatchers = mutableSetOf<Regex>()
+
+    init {
+        if (scenariosSelectors.isPresent && scenariosSelectors.get().isNotBlank()) {
+            scenariosSelectors.get().split(",").filter { it.isNotBlank() }.forEach {
+                if (it.contains('*') || it.contains('?')) {
+                    patternMatchers.add(Regex(it.replace('?', '.').replace("*", ".*")))
+                } else {
+                    exactMatchers.add(it.trim())
+                }
+            }
+        }
     }
 
+    override fun asMap(): Map<ScenarioId, ConfiguredScenarioSpecification> {
+        return filterScenarios(scenariosSpecifications)
+    }
+
+    /**
+     * Filters out the scenarios that are not matching the existing selectors. When no selector is set, [scenarios] is returned.
+     */
+    // Visible for test.
+    internal fun filterScenarios(scenarios: Map<ScenarioId, ConfiguredScenarioSpecification>): Map<ScenarioId, ConfiguredScenarioSpecification> {
+        return if (exactMatchers.isEmpty() && patternMatchers.isEmpty()) {
+            scenarios
+        } else scenarios.filterKeys { scenarioId ->
+            exactMatchers.contains(scenarioId) || patternMatchers.any { pattern -> pattern.matches(scenarioId) }
+        }
+    }
 }
--- runtime/src/testFixtures/kotlin/io/qalipsis/runtime/test/QalipsisTestRunner.kt
+++ runtime/src/testFixtures/kotlin/io/qalipsis/runtime/test/QalipsisTestRunner.kt
@@ -3,14 +3,82 @@ package io.qalipsis.runtime.test
 import io.qalipsis.runtime.Qalipsis
 
 /**
+ * Utils to execute scenarios in a concrete runtime.
  *
  * @author Eric Jessé
  */
-object QalipsisTestRunner {
+object QalipsisTestRunner : ConfigurableQalipsisTestRunner {
+
+    override fun withScenarios(vararg scenarios: String): ConfigurableQalipsisTestRunner {
+        return ConfigurableQalipsisTestRunnerImpl().withScenarios(*scenarios)
+    }
+
+    override fun withEnvironments(vararg environments: String): ConfigurableQalipsisTestRunner {
+        return ConfigurableQalipsisTestRunnerImpl().withEnvironments(*environments)
+    }
+
+    override fun execute(vararg args: String) = ConfigurableQalipsisTestRunnerImpl().execute(*args)
+
+}
+
+/**
+ * Runner to execute scenarios in a concrete runtime.
+ */
+interface ConfigurableQalipsisTestRunner {
 
     /**
-     * Starts a local standalone qalipsis process with the test profile enabled.
+     * Restricts the executed scenarios to the list passed as parameter.
+     * It support wildcards such as * and ?.
      */
-    fun execute(vararg args: String) = Qalipsis.start(arrayOf("-e", "test").plus(args))
+    fun withScenarios(vararg scenarios: String): ConfigurableQalipsisTestRunner
+
+    /**
+     * Enables the environments for the execution.
+     */
+    fun withEnvironments(vararg environments: String): ConfigurableQalipsisTestRunner
+
+    /**
+     * Starts a local standalone qalipsis process with the test profile enabled and the passed arguments.
+     */
+    fun execute(vararg args: String): Int
+
+}
+
+internal class ConfigurableQalipsisTestRunnerImpl : ConfigurableQalipsisTestRunner {
+
+    val scenarios = mutableListOf<String>()
+
+    val environments = mutableListOf("config")
+
+    override fun withScenarios(vararg scenarios: String): ConfigurableQalipsisTestRunner {
+        this.scenarios.addAll(scenarios.toList())
+        return this
+    }
+
+    override fun withEnvironments(vararg environments: String): ConfigurableQalipsisTestRunner {
+        this.environments.addAll(environments.toList())
+        return this
+    }
+
+    override fun execute(vararg args: String): Int {
+        val allArgs = mutableListOf<String>()
+        if (scenarios.isNotEmpty()) {
+            allArgs.add("-s")
+            allArgs.add(scenarios.joinToString(separator = ",", transform = String::trim))
+        }
+
+        allArgs.add("-e")
+        allArgs.add("test")
+        environments.forEach {
+            allArgs.add("-e")
+            allArgs.add(it.trim())
+            // Also adds each env with the suffix -test.
+            allArgs.add("-e")
+            allArgs.add("${it.trim()}-test")
+        }
+        allArgs.addAll(args.toList())
+
+        return Qalipsis.start(allArgs.toTypedArray())
+    }
 
 }
