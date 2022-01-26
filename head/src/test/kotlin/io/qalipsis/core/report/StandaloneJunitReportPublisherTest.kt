ed6ef9d400c2a06d8c6a308cd73a257801bbb047
Eric Jessé <me@ericjesse.com>
2022-01-26 22:43:49 +0100

    QALI-88 - Generate JUnit like report files.
    Make the test of publishOnLeave lighter and stronger.

--- head/src/test/kotlin/io/qalipsis/core/report/StandaloneJunitReportPublisherTest.kt
+++ head/src/test/kotlin/io/qalipsis/core/report/StandaloneJunitReportPublisherTest.kt
@@ -1,10 +1,13 @@
 package io.qalipsis.core.report
 
+import assertk.assertThat
+import assertk.assertions.isGreaterThan
 import io.mockk.coEvery
 import io.mockk.coVerifyOrder
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.mockkStatic
+import io.mockk.spyk
 import io.qalipsis.api.report.CampaignReport
 import io.qalipsis.api.report.CampaignStateKeeper
 import io.qalipsis.api.report.ExecutionStatus
@@ -14,12 +17,15 @@ import io.qalipsis.api.report.ScenarioReport
 import io.qalipsis.core.head.campaign.CampaignConfiguration
 import io.qalipsis.test.coroutines.TestDispatcherProvider
 import io.qalipsis.test.mockk.WithMockk
+import io.qalipsis.test.time.coMeasureTime
+import kotlinx.coroutines.delay
 import org.junit.Assert
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 import java.io.File
 import java.time.Clock
+import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
 
@@ -49,7 +55,12 @@ internal class StandaloneJunitReportPublisherTest {
     fun `should write simple report without errors`() = testCoroutineDispatcher.run {
         //given
         val standaloneJunitReportPublisher =
-            StandaloneJunitReportPublisher(campaignConfiguration, campaignStateKeeper, testCoroutineContext, generatedReportFolder)
+            StandaloneJunitReportPublisher(
+                campaignConfiguration,
+                campaignStateKeeper,
+                testCoroutineContext,
+                generatedReportFolder
+            )
 
         val campaignId = "foo"
 
@@ -111,7 +122,12 @@ internal class StandaloneJunitReportPublisherTest {
     fun `should write report with errors`() = testCoroutineDispatcher.run {
         //given
         val standaloneJunitReportPublisher =
-            StandaloneJunitReportPublisher(campaignConfiguration, campaignStateKeeper, testCoroutineContext, generatedReportFolder)
+            StandaloneJunitReportPublisher(
+                campaignConfiguration,
+                campaignStateKeeper,
+                testCoroutineContext,
+                generatedReportFolder
+            )
 
         val campaignId = "foo"
 
@@ -181,7 +197,12 @@ internal class StandaloneJunitReportPublisherTest {
     fun `should write few reports with errors`() = testCoroutineDispatcher.run {
         //given
         val standaloneJunitReportPublisher =
-            StandaloneJunitReportPublisher(campaignConfiguration, campaignStateKeeper, testCoroutineContext, generatedReportFolder)
+            StandaloneJunitReportPublisher(
+                campaignConfiguration,
+                campaignStateKeeper,
+                testCoroutineContext,
+                generatedReportFolder
+            )
 
         val campaignId1 = "foo"
         val campaignId2 = "foo2"
@@ -275,52 +296,30 @@ internal class StandaloneJunitReportPublisherTest {
     }
 
     @Test
-    fun `should call publish and wait for the full execution`() {
+    fun `should call publish and wait for the full execution on leave`() {
         //given
         val standaloneJunitReportPublisher =
-            StandaloneJunitReportPublisher(campaignConfiguration, campaignStateKeeper, testCoroutineContext, generatedReportFolder)
-
+            spyk(
+                StandaloneJunitReportPublisher(
+                    campaignConfiguration,
+                    campaignStateKeeper,
+                    testCoroutineContext,
+                    generatedReportFolder
+                ), recordPrivateCalls = true
+            )
         val campaignId = "boo"
-
-        val timeDiffSeconds = 10L
-        val start = Instant.now().minusSeconds(timeDiffSeconds)
-        val end = Instant.now()
-
-        val campaignReport = CampaignReport(
-            campaignId = campaignId, start = start, end = end, scenariosReports = listOf(
-                ScenarioReport(
-                    campaignId = campaignId,
-                    scenarioId = "bar0",
-                    start = start,
-                    end = end,
-                    configuredMinionsCount = 1,
-                    executedMinionsCount = 1,
-                    stepsCount = 1,
-                    successfulExecutions = 1,
-                    failedExecutions = 1,
-                    status = ExecutionStatus.SUCCESSFUL,
-                    messages = listOf(
-                        ReportMessage(
-                            stepId = "normal test",
-                            messageId = 1,
-                            severity = ReportMessageSeverity.INFO,
-                            message = "passed"
-                        )
-                    )
-                )
-            ), status = ExecutionStatus.SUCCESSFUL
-        )
-
+        coEvery { standaloneJunitReportPublisher.publish(any()) } coAnswers { delay(300) }
         coEvery { campaignConfiguration.id } returns campaignId
-        coEvery { campaignStateKeeper.report(campaignId) } returns campaignReport
 
         //when
-        standaloneJunitReportPublisher.publishOnLeave()
+        val elapsedTime = coMeasureTime {
+            standaloneJunitReportPublisher.publishOnLeave()
+        }
 
         //then
+        assertThat(elapsedTime).isGreaterThan(Duration.ofMillis(250))
         coVerifyOrder {
-            campaignConfiguration.id
-            campaignStateKeeper.report(campaignId)
+            standaloneJunitReportPublisher.publish(campaignId)
         }
     }
 
