7a681328ad00e4f292dc1f69b94430f3dd6fc569
sandro <ssmamukelashvili@gmail.com>
2022-04-18 22:04:25 +0400

    check length of tenant's display name

--- head/src/main/kotlin/io/qalipsis/core/head/admin/AdminController.kt
+++ head/src/main/kotlin/io/qalipsis/core/head/admin/AdminController.kt
@@ -5,15 +5,15 @@ import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 
-@Controller("api/admin")
+@Controller("/api/admin")
 class AdminController(
     private val tenantManagement: TenantManagement
 ) {
-
-
-
     @Post("tenant")
     suspend fun createTenant(@Body saveTenantDto: SaveTenantDto): HttpResponse<Any> {
+        if (saveTenantDto.displayName.length > 200 || saveTenantDto.displayName.isEmpty()) {
+            throw TenantDisplayNameLengthException()
+        }
         return HttpResponse.ok(tenantManagement.saveTenant(saveTenantDto))
     }
 }
