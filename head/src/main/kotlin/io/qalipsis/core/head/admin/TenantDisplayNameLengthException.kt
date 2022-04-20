d56b2e764810354d197fa20e75524dbd374e462a
sandro <ssmamukelashvili@gmail.com>
2022-04-20 09:39:53 +0400

    Revert "check length of tenant's display name"
    This reverts commit 7a681328ad00e4f292dc1f69b94430f3dd6fc569.

--- head/src/main/kotlin/io/qalipsis/core/head/admin/TenantDisplayNameLengthException.kt
+++
@@ -1,8 +0,0 @@
-package io.qalipsis.core.head.admin
-
-/**
- * Exception to throw when tenant display name length is greater than 200 or less than 1.
- *
- * @author Sandro Mamukelashvili
- */
-class TenantDisplayNameLengthException : RuntimeException()
