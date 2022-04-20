3b7a1140508438188660c0a041734f70683cdc9a
sandro <ssmamukelashvili@gmail.com>
2022-04-20 09:08:05 +0400

    Revert "add integration tests"
    This reverts commit aa66c4c19a28b75a1735e61b12e593709cf64f93.

--- head/build.gradle.kts
+++ head/build.gradle.kts
@@ -53,8 +53,6 @@ dependencies {
     implementation(platform("io.micronaut:micronaut-bom:${micronautVersion}"))
 
     implementation(project(":core"))
-    implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation ("com.google.code.gson:gson:2.9.0")
     implementation("io.qalipsis:api-common:${project.version}")
     implementation("io.qalipsis:api-dsl:${project.version}")
     implementation("io.qalipsis:api-processors:${project.version}")
