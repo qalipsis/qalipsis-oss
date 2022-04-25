56fdc419e2db10690ba9e0ec165fddfbe5c1f750
p-bril <98779568+p-bril@users.noreply.github.com>
2022-04-25 12:16:28 +0300

    Feature/qali 204 user retrieval update deletion (#60)
    QALI-204 User retrieval update deletion

---
+++ head/src/main/kotlin/io/qalipsis/core/head/security/impl/UserManagementImpl.kt
@@ -0,0 +1,45 @@
+package io.qalipsis.core.head.security.impl
+
+import io.qalipsis.core.head.jdbc.entity.UserEntity
+import io.qalipsis.core.head.jdbc.repository.UserRepository
+import io.qalipsis.core.head.security.IdentityManagement
+import io.qalipsis.core.head.security.UserManagement
+import io.qalipsis.core.head.security.UserPatch
+import java.time.Instant
+
+/**
+ * There is a default implementation of [UserManagement] interface that encapsulate
+ * the “identity management” enabled by configuration
+ *
+ * @author Palina Bril
+ */
+class UserManagementImpl(
+    private val identityManagement: IdentityManagement,
+    private val userRepository: UserRepository
+) : UserManagement {
+
+    override suspend fun get(username: String): UserEntity? {
+        val userEntity = userRepository.findByUsername(username)
+        return if (userEntity?.disabled == null) {
+            userEntity
+        } else {
+            null
+        }
+    }
+
+    override suspend fun save(user: UserEntity, userPatches: Collection<UserPatch>) {
+        if (userPatches.asSequence().map { it.apply(user) }.any()) {
+            identityManagement.update(user)
+            userRepository.update(user)
+        }
+    }
+
+    override suspend fun delete(username: String) {
+        val userEntity = userRepository.findByUsername(username)
+        if (userEntity != null) {
+            val disabledUser = userEntity.copy(disabled = Instant.now())
+            userRepository.update(disabledUser)
+            identityManagement.update(disabledUser)
+        }
+    }
+}
