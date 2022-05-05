488cab94e1c5af2192c290df274f99d414e9421c
Palina Bril <venera_48@mail.ru>
2022-05-05 12:22:11 +0300

    QALI-177 Maintain the roles of a user in a tenant

--- head/src/main/kotlin/io/qalipsis/core/head/security/auth0/Auth0PatchConverter.kt
+++ head/src/main/kotlin/io/qalipsis/core/head/security/auth0/Auth0PatchConverter.kt
@@ -1,6 +1,5 @@
 package io.qalipsis.core.head.security.auth0
 
-import com.auth0.json.mgmt.users.User
 import io.qalipsis.core.head.security.AddRoleUserPatch
 import io.qalipsis.core.head.security.Auth0Patch
 import io.qalipsis.core.head.security.DeleteRoleUserPatch
@@ -26,35 +25,12 @@ internal class Auth0PatchConverter(private val userPatches: Collection<UserPatch
                 is UsernameUserPatch -> UsernameAuth0Patch(it.newUsername)
                 is EmailAddressUserPatch -> EmailAuth0Patch(it.newEmailAddress)
                 is DisplayNameUserPatch -> NameAuth0Patch(it.newDisplayName)
-                is AddRoleUserPatch -> { if(it.applied) RoleAuth0Patch() else null}
+                is AddRoleUserPatch -> {
+                    if (it.applied) RoleAuth0Patch() else null
+                }
                 is DeleteRoleUserPatch -> RoleAuth0Patch()
                 else -> throw NoSuchElementException("No such userPatch exist - ${it}")
             }
         }.filterNotNull().toList()
-    }
-
-    fun convert1(userPatches: List<UserPatch>): List<User> {
-        val user = User()
-        userPatches.forEach {
-            if (it is UsernameUserPatch) {
-                user.username = it.newUsername
-            }
-            if (it is EmailAddressUserPatch) {
-                user.email = it.newEmailAddress
-            }
-            if (it is DisplayNameUserPatch) {
-                user.name = it.newDisplayName
-            }
-        }
-        if (user.username.isNotEmpty() && user.email.isNotEmpty()) {
-            val userOne = User()
-            userOne.username = user.username
-            userOne.name = user.name
-            val userTwo = User()
-            userTwo.email = user.email
-            return mutableListOf(userOne, userTwo)
-        } else {
-            return mutableListOf(user)
-        }
     }
 }
