dd8398e0f04de799df6e3a0caa84f97f3e43fa19
Eric Jessé <me@ericjesse.com>
2022-08-12 18:08:58 +0200

    Minor renaming for the events and meters configuration constants

---
+++ api-dev/src/main/kotlin/io/qalipsis/api/config/EventsConfig.kt
@@ -0,0 +1,16 @@
+package io.qalipsis.api.config
+
+/**
+ * Configuration facilities for the generation of events.
+ *
+ * @author Eric Jessé
+ */
+object EventsConfig {
+
+    const val EXPORT_CONFIGURATION = "events.export"
+
+    const val EXPORT_ENABLED = "$EXPORT_CONFIGURATION.enabled"
+
+    const val PROVIDER_CONFIGURATION = "events.provider"
+
+}
---
+++ api-dev/src/main/kotlin/io/qalipsis/api/config/MetersConfig.kt
@@ -0,0 +1,16 @@
+package io.qalipsis.api.config
+
+/**
+ * Configuration facilities for the generation of meters.
+ *
+ * @author Eric Jessé
+ */
+object MetersConfig {
+
+    const val EXPORT_CONFIGURATION = "meters.export"
+
+    const val EXPORT_ENABLED = "$EXPORT_CONFIGURATION.enabled"
+
+    const val PROVIDER_CONFIGURATION = "meters.provider"
+
+}
