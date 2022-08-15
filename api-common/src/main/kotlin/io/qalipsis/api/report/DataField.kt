b78a4ba7cf4da8ce6383d31f8cb85e7b18adf2b1
Eric Jessé <me@ericjesse.com>
2022-08-15 15:12:51 +0200

    Fixes on the time-series queries definitions

--- api-common/src/main/kotlin/io/qalipsis/api/report/DataField.kt
+++ api-common/src/main/kotlin/io/qalipsis/api/report/DataField.kt
@@ -1,16 +1,23 @@
 package io.qalipsis.api.report
 
 /**
- * Field of a data source.
+ * Field of a time-series data source.
  *
  * @author Eric Jessé
  *
  * @property name name of the field in the data source
- * @property number specifies whether the field can be used in aggregations
- * @property unit the unit of the values, if relevant (durations,...)
+ * @property type type for the
+ * @property unit the unit of the values, if relevant (durations,...) and not specified in the records
  */
 data class DataField(
     val name: String,
-    val number: Boolean,
+    val type: DataFieldType,
     val unit: String? = null
 )
+
+/**
+ * Type of a field of time-series data.
+ */
+enum class DataFieldType {
+    STRING, NUMBER, BOOLEAN, OBJECT, DATE
+}
--- api-common/src/main/kotlin/io/qalipsis/api/report/query/QueryClauseOperator.kt
+++ api-common/src/main/kotlin/io/qalipsis/api/report/query/QueryClauseOperator.kt
@@ -9,5 +9,5 @@ import io.micronaut.core.annotation.Introspected
  */
 @Introspected
 enum class QueryClauseOperator {
-    IS, IS_NOT, IS_IN, IS_NOT_IN, LIKE, IS_NOT_LIKE, IS_GREATER_THAN, IS_LOWER_THAN, IS_GREATER_OR_EQUAL_TO, IS_LOWER_OR_EQUAL_TO
+    IS, IS_NOT, IS_IN, IS_NOT_IN, IS_LIKE, IS_NOT_LIKE, IS_GREATER_THAN, IS_LOWER_THAN, IS_GREATER_OR_EQUAL_TO, IS_LOWER_OR_EQUAL_TO
 }
