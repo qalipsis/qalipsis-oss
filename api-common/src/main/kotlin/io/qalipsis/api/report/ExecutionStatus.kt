71320c3a13dff1975fe62549a2740c2a2dacb61d
Eric Jessé <me@ericjesse.com>
2022-08-26 11:36:29 +0200

    Additional execution status

--- api-common/src/main/kotlin/io/qalipsis/api/report/ExecutionStatus.kt
+++ api-common/src/main/kotlin/io/qalipsis/api/report/ExecutionStatus.kt
@@ -7,6 +7,9 @@ package io.qalipsis.api.report
  * @property WARNING a deeper look at the reports is required, but the campaign does not fail
  * @property FAILED the campaign went until the end, but got errors
  * @property ABORTED the campaign was aborted, either by a user or a critical failure
+ * @property SCHEDULED the campaign is scheduled for a later point in time
+ * @property QUEUED the campaign is being prepared and will start very soon
+ * @property IN_PROGRESS the campaign is currently running
  *
  * @author Eric Jessé
  */
@@ -14,5 +17,8 @@ enum class ExecutionStatus(val exitCode: Int) {
     SUCCESSFUL(0),
     WARNING(0),
     FAILED(1),
-    ABORTED(2)
+    ABORTED(2),
+    SCHEDULED(-1),
+    QUEUED(-1),
+    IN_PROGRESS(-1)
 }
