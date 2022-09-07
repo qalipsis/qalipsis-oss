719a6d4be5c6a313a7bfc36e2285b0c65b0066c5
Eric Jessé <me@ericjesse.com>
2022-09-07 11:45:13 +0200

    Fixes the deployment

---
+++ deployment/config/postgres/0_init-schema.sql
@@ -0,0 +1,6 @@
+CREATE SCHEMA IF NOT EXISTS qalipsis_liquibase;
+ALTER
+SCHEMA qalipsis_liquibase OWNER TO qalipsis;
+CREATE SCHEMA IF NOT EXISTS qalipsis;
+ALTER
+SCHEMA qalipsis OWNER TO qalipsis;
--- deployment/config/postgres/init-schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA qalipsis AUTHORIZATION qalipsis;
-GRANT
-ALL
-ON SCHEMA qalipsis TO qalipsis;
-
-CREATE SCHEMA qalipsis_liquibase AUTHORIZATION qalipsis;
-GRANT
-ALL
-ON SCHEMA qalipsis_liquibase TO qalipsis;
