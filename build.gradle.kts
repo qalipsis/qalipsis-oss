6cecee84456ccdbe504144cabfe3957455fcd389
Eric Jessé <me@ericjesse.com>
2023-02-17 22:54:20 +0100

    Fixes the JAR names

--- build.gradle.kts
+++ build.gradle.kts
@@ -154,7 +154,7 @@ fun Project.configureNotPlatform() {
 
     tasks {
         withType<Jar> {
-            archiveBaseName.set("com-qalipsis-${project.name}")
+            archiveBaseName.set("io-qalipsis-${project.name}")
         }
 
         withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
