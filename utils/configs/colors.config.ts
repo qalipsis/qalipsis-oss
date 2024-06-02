2ddf7654873b80dff8174ea5f30460e2d497013c
Kuan-Min Wang <kuanmin.wang@gmail.com>
2024-06-02 12:36:08 +0200

    [bugfix] Fix required node js version not applied issue

--- utils/configs/colors.config.ts
+++ utils/configs/colors.config.ts
@@ -1,5 +1,5 @@
 export class ColorsConfig {
-    static GREY_2_HEX_CODE = "#d5d8d9";
+    static GREY_2_HEX_CODE = "#dcdfdf";
     static PURPLE_COLOR_HEX_CODE = "#8c3cee";
     static PRIMARY_COLOR_HEX_CODE = "#41c9ca";
     static PINK_HEX_CODE = "#f33278";
